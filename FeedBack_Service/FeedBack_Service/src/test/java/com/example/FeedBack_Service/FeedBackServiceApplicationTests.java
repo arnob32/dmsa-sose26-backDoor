f95cac83aefbf505b964f27154bceccfef79e000
Raju <rajunaidu.bd@gmail.com>
2026-06-18 11:33:33 +0200

    Initial commit - all 6 microservices

---
+++ FeedBack_Service/FeedBack_Service/src/test/java/com/example/FeedBack_Service/FeedBackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FeedBack_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedBackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ IssueReport_Service/IssueReport_Service/src/main/java/com/example/IssueReport_Service/IssueReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.IssueReport_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueReportServiceApplication.class, args);
+	}
+
+}
